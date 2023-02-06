9108177162056b929981f572b40b697981f80398
mrmdv <mrmdv@ua.fm>
2023-02-06 22:10:00 +0200

    add highlight the current user

--- js/main.js
+++ js/main.js
@@ -29,10 +29,10 @@ document.addEventListener('swiped-right', function (e) {
 });
 
 for (const el of scheduleBtns) {
-   el.onclick = () => {
-      scheduleBtn = el.dataset.responsibility;
-      tableRendering(scheduleBtn, "scheduleBody", apiData[scheduleBtn]);
-   };
+    el.onclick = () => {
+        scheduleBtn = el.dataset.responsibility;
+        tableRendering(scheduleBtn, "scheduleBody", apiData[scheduleBtn], apiData.currentUser);
+    };
 }
 // Get today's schedule
 const todayBtn = document.getElementById("todayBtn");
@@ -89,10 +89,7 @@ function getSchedule(params) {
             if (scheduleBtn == undefined) {
                 scheduleBtn = "securityShift";
             }
-            tableRendering(scheduleBtn, "scheduleBody", data[scheduleBtn]);
-        //    setTimeout(() => {
-        //        hiddenSwitching("preloaderSec", "schedule");
-        //    }, 1000);
+            tableRendering(scheduleBtn, "scheduleBody", data[scheduleBtn], data.currentUser);
         })
         .then(() => {
             reorderHidden(["schedule"], ["preloaderSec"]);
@@ -103,33 +100,36 @@ function getSchedule(params) {
         });
 }
 
-function tableRendering(type, table, data) {
-   if (data) {
-      var pageHeaderHtml = `<h2>Розклад з <span id="dateFrom">${dateFormta(
-         data.dateFrom
-      )}</span> по <span id="dateTo">${dateFormta(data.dateTo)}</span></h2>`;
-      document.getElementById(table).innerHTML = pageHeaderHtml;
+function tableRendering(type, table, data, currentUser) {
+    if (data) {
+        var pageHeaderHtml = `<h2>Розклад з <span id="dateFrom">${dateFormta(
+            data.dateFrom
+        )}</span> по <span id="dateTo">${dateFormta(data.dateTo)}</span></h2>`;
+        document.getElementById(table).innerHTML = pageHeaderHtml;
 
-      switch (type) {
-         case "dutyShift":
-            renderPart(table, "Черговий", data.chief);
-            renderPart(table, "Днювальний (охорона)", data.dutySecurity);
-            renderPart(table, "Днювальний (прибирання)", data.dutyCleaning);
-            renderPart(table, "Черговий по кухні", data.dutyKitchen);
+        switch (type) {
+            case "dutyShift":
+                renderPart(table, "Черговий", data.chief, currentUser);
+                renderPart(table, "Днювальний (охорона)", data.dutySecurity, currentUser);
+                renderPart(table, "Днювальний (прибирання)", data.dutyCleaning, currentUser);
+                renderPart(table, "Черговий по кухні", data.dutyKitchen, currentUser);
 
-            renderPartOneColumn(table, "В розташуванні", data.atBase);
+                renderPartOneColumn(table, "В розташуванні", data.atBase, currentUser);
 
-            break;
+                break;
 
-         case "securityShift":
-            var headerHtml = `<div class="tableWrap" id="tableWrap">
+            case "securityShift":
+                var headerHtml = `<div class="tableWrap" id="tableWrap">
 						<div id="tableBody">`;
 
-            var chiefHtml = `   <div class="tRow df" >
+                var addClass = "";
+                if (data.chief === currentUser) addClass = " currentUser";
+
+                var chiefHtml = `   <div class="tRow df" >
 							<div class="rowItem rowChiefDesc" id="chiefPos">
 								Начальник варти
 							</div>
-							<div class="rowItem rowChief" id="chief">
+							<div class="rowItem rowChief${addClass}" id="chief">
 								${data.chief}
 							</div>
 						</div>
@@ -142,47 +142,50 @@ function tableRendering(type, table, data) {
 							</div>
 						</div>`;
 
-            document.getElementById(table).innerHTML += headerHtml + chiefHtml;
+                document.getElementById(table).innerHTML += headerHtml + chiefHtml;
 
-            data.duties.forEach((element) => {
-               let guard = "";
-               element.guard.forEach((el, index, array) => {
-                  guard += " ";
-                  guard += el.split(" ")[0];
-                  if (index != array.length - 1) {
-                     guard += "<br>";
-                  }
-               });
-               let rowTemplate = `  
+                data.duties.forEach((element) => {
+                    let guard = "";
+                    element.guard.forEach((el, index, array) => {
+                        var isCurrent = el === currentUser;
+                        guard += " ";
+                        if (isCurrent) { guard += "<em><strong>"; }                       
+                        guard += el.split(" ")[0];
+                        if (isCurrent) { guard += "</strong></em>"; }                       
+                        if (index != array.length - 1) {
+                            guard += "<br>";
+                        }
+                    });
+                    let rowTemplate = `  
                      <div class="tRow df">
                         <div class="rowTimeDesc rowItem">
                            ${element.period}
                         </div>
-                        <div class="rowGuardDesc rowItem">
+                        <div class="rowGuardDesc rowItem${addClass}">
                            ${guard}
                         </div>
                      </div>`;
 
-               document.getElementById(table).innerHTML += rowTemplate;
-            });
+                    document.getElementById(table).innerHTML += rowTemplate;
+                });
 
-            var footerHtml = `       </div>
+                var footerHtml = `       </div>
 					     </div>`;
-            document.getElementById(table).innerHTML += footerHtml;
-            break;
+                document.getElementById(table).innerHTML += footerHtml;
+                break;
 
-         case "vacationShift":
-            renderPartOneColumn(table, "В резерві", data.reserve);
-            renderPartOneColumn(table, "Відпустка", data.onVacation);
-            break;
-      }
-   } else {
-      document.getElementById(table).innerHTML = "";
-   }
+            case "vacationShift":
+                renderPartOneColumn(table, "В резерві", data.reserve, currentUser);
+                renderPartOneColumn(table, "Відпустка", data.onVacation, currentUser);
+                break;
+        }
+    } else {
+        document.getElementById(table).innerHTML = "";
+    }
 }
 
-function renderPart(table, header, data) {
-   var headerHtml = `<h2>${header}</h2>
+function renderPart(table, header, data, currentUser) {
+    var headerHtml = `<h2>${header}</h2>
             <div class="tableWrap" id="tableWrap">
 			<div id="tableBody">
                 <div class="tRow df">
@@ -193,29 +196,32 @@ function renderPart(table, header, data) {
 						ПІБ
 					</div>
 				</div>`;
-   document.getElementById(table).innerHTML += headerHtml;
+    document.getElementById(table).innerHTML += headerHtml;
 
-   data.forEach((element) => {
-      let rowTemplate = `  
+    data.forEach((element) => {
+        var addClass = "";
+        if (element.person === currentUser) addClass = " currentUser";
+
+        var rowTemplate = `  
                      <div class="tRow df">
                         <div class="rowTimeDesc rowItem">
                            ${element.period}
                         </div>
-                        <div class="rowGuardDesc rowItem">
+                        <div class="rowGuardDesc rowItem${addClass}">
                            ${element.person}
                         </div>
                      </div>`;
 
-      document.getElementById(table).innerHTML += rowTemplate;
-   });
+        document.getElementById(table).innerHTML += rowTemplate;
+    });
 
-   var footerHtml = `       </div>
+    var footerHtml = `       </div>
             </div>`;
-   document.getElementById(table).innerHTML += footerHtml;
+    document.getElementById(table).innerHTML += footerHtml;
 }
 
-function renderPartOneColumn(table, header, data) {
-   var headerHtml = `<h2>${header}</h2>
+function renderPartOneColumn(table, header, data, currentUser) {
+    var headerHtml = `<h2>${header}</h2>
         <div class="tableWrap" id="tableWrap">
 		<div id="tableBody">
             <div class="tRow df">
@@ -223,22 +229,25 @@ function renderPartOneColumn(table, header, data) {
 					ПІБ
 				</div>
 			</div>`;
-   document.getElementById(table).innerHTML += headerHtml;
+    document.getElementById(table).innerHTML += headerHtml;
 
-   data.forEach((element) => {
-      let rowTemplate = `  
+    data.forEach((element) => {
+        var addClass = "";
+        if (element === currentUser) addClass = " currentUser";
+
+        var rowTemplate = `  
                      <div class="tRow df">
-                        <div class="rowGuardDesc rowItem">
+                        <div class="rowGuardDesc rowItem${addClass}">
                            ${element}
                         </div>
                      </div>`;
 
-      document.getElementById(table).innerHTML += rowTemplate;
-   });
+        document.getElementById(table).innerHTML += rowTemplate;
+    });
 
-   var footerHtml = `       </div>
+    var footerHtml = `       </div>
         </div>`;
-   document.getElementById(table).innerHTML += footerHtml;
+    document.getElementById(table).innerHTML += footerHtml;
 }
 
 function renderData(id, data) {
