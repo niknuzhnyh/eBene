a2ca71884df1f788e5a4f85d1f5feef6a4b85313
Nikolay Nuzhnyh <niknuzhnyh@gmail.com>
2023-02-22 01:06:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -531,7 +531,7 @@ if (navigator.serviceWorker.controller) {
       .then(function (reg) {
          // console.log(
          //    "Service worker has been registered for scope:" + reg.scope
-         );
+         // );
       });
 }
 
